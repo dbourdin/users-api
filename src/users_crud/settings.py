b94f961d3f8ebce8a2b9daaafd500b5f1811866f
Damian Bourdin <damian.bourdin@gmail.com>
2022-04-20 23:28:27 +0200

    Added Settings using FastAPI BaseSettings.

---
+++ src/users_crud/settings.py
@@ -0,0 +1,73 @@
+"""API Settings."""
+
+from enum import Enum
+from functools import lru_cache
+from ipaddress import IPv4Address
+from typing import Any
+
+from pydantic import BaseSettings, PositiveInt
+
+from users_crud.version import __version__
+
+
+class EnvironmentEnum(str, Enum):
+    """API Environment Enum."""
+
+    production = "production"
+    development = "development"
+
+
+class LoggingEnum(str, Enum):
+    """Logging configuration Enum."""
+
+    critical = "CRITICAL"
+    error = "ERROR"
+    warning = "WARNING"
+    info = "INFO"
+    debug = "DEBUG"
+
+
+class APISettings(BaseSettings):
+    """Basic API settings.
+
+    Includes database configuration, environment, CORS and logging, among others.
+    """
+
+    ENVIRONMENT: EnvironmentEnum = EnvironmentEnum.production
+
+    API_VERSION: str = __version__
+
+    LOGLEVEL: LoggingEnum = LoggingEnum.info
+
+    # Settings related to running an ASGI app
+    APP_MODULE: str = "users_crud.app:app"
+    HOST: IPv4Address = "0.0.0.0"
+    PORT: PositiveInt = 3000
+
+    def get_uvicorn_settings(self) -> dict[str, Any]:
+        """Get a dictionary with settings ready to be used by Uvicorn."""
+        return {
+            "app": self.APP_MODULE,
+            "host": str(self.HOST),
+            "port": self.PORT,
+            "log_level": self.LOGLEVEL.lower(),  # Uvicorn expects lowercase strings
+            "reload": True,
+        }
+
+    # To use the API behind a proxy, set this variable to the desired base route
+    # This will make the /docs URL work properly
+    # More info here: https://fastapi.tiangolo.com/advanced/behind-a-proxy/
+    ROOT_PATH: str = ""
+
+
+class Settings(APISettings):
+    """API settings.
+
+    Includes configuration tied specifically to this API.
+    """
+
+
+@lru_cache(maxsize=1)
+def get_settings() -> Settings:
+    """Get the API Settings."""
+    return Settings()
