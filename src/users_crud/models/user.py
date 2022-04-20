5a244a5a7a2958366b0422566832b7e5c4f057d0
Damian Bourdin <damian.bourdin@gmail.com>
2022-04-20 23:19:06 +0200

    Added base psasword hashing to User.

--- src/users_crud/models/user.py
+++ src/users_crud/models/user.py
@@ -1,5 +1,6 @@
 """User database table."""
 
+import bcrypt
 from sqlalchemy import Column, String
 
 from users_crud.db.base_class import Base
@@ -11,3 +12,23 @@ class User(Base):
     username = Column(String, nullable=False)
     first_name = Column(String)
     last_name = Column(String)
+    password_hash = Column(String)
+
+    @property
+    def password(self):
+        """Block password from being read."""
+        raise AttributeError("password is not a readable attribute!")
+
+    @password.setter
+    def password(self, password):
+        """Password hashing."""
+        salt = bcrypt.gensalt()
+        if isinstance(password, str):
+            password = password.encode("utf8")
+        self.password_hash = bcrypt.hashpw(password, salt)
+
+    def verify_password(self, password):
+        """Password validator."""
+        if isinstance(password, str):
+            password = password.encode("utf8")
+        return bcrypt.checkpw(password, self.password_hash)
