74c9a9154b96d452a3a77f05c97f13dfcfdc2669
Damian Bourdin <damian.bourdin@gmail.com>
2022-04-21 04:14:21 +0200

    Added schemas to __init__.

--- src/users_crud/schemas/__init__.py
+++ src/users_crud/schemas/__init__.py
@@ -1,2 +1,11 @@
 from users_crud.schemas.api import ApiVersionModel
 from users_crud.schemas.base import APIMessage
+from users_crud.schemas.user import (
+    UserCreateDB,
+    UserCreateIn,
+    UserCreateOut,
+    UserList,
+    UserUpdateDB,
+    UserUpdateIn,
+    UserUpdateOut,
+)
