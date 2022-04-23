8a0d31a466d43eda6cd70fdbc6cbea15e7ae2149
Damian Bourdin <damian.bourdin@gmail.com>
2022-04-23 07:32:37 +0200

    Added login endpoint.

---
+++ src/users_crud/api/v1/endpoints/auth.py
@@ -0,0 +1,50 @@
+"""Endpoints related to Authentication."""
+
+from datetime import timedelta
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+
+from users_crud import crud, schemas
+from users_crud.api import deps
+from users_crud.api.security import create_access_token
+from users_crud.models.user import User
+
+router = APIRouter()
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+@router.post(
+    "/login",
+    response_model=schemas.Token,
+    summary="Login",
+    description="Login",
+)
+async def login_for_access_token(
+    db: Session = Depends(deps.get_db), form_data: OAuth2PasswordRequestForm = Depends()
+) -> schemas.Token:
+    """Retrieve an existing User.
+
+    Args:
+        db (Session): A database session
+        form_data (OAuth2PasswordRequestForm): form data containing User credentials
+
+    Raises:
+        HTTPException: If the User cannot be authenticated
+    """
+    user: User = crud.user.authenticate(
+        db, username=form_data.username, password=form_data.password
+    )
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.uuid}, expires_delta=access_token_expires
+    )
+    return schemas.Token(access_token=access_token, token_type="bearer")
--- src/users_crud/api/v1/routers.py
+++ src/users_crud/api/v1/routers.py
@@ -2,7 +2,8 @@
 
 from fastapi import APIRouter
 
-from users_crud.api.v1.endpoints import users
+from users_crud.api.v1.endpoints import auth, users
 
 router = APIRouter()
 router.include_router(users.router, prefix="/users", tags=["users"])
+router.include_router(auth.router, prefix="/auth", tags=["auth"])
