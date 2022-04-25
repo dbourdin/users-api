8b0a74ed8fb36d28efc12584f29fa474db8916ff
Damian Bourdin <damian.bourdin@gmail.com>
2022-04-25 10:20:23 +0200

    Added settings test and version validation.

--- tests/test_base.py
+++ tests/test_base.py
@@ -3,10 +3,20 @@
 from fastapi import status
 from fastapi.testclient import TestClient
 
+from users_api import version
 
-def test_import(client: TestClient):
-    """Basic FastAPI test."""
+
+def test_version(client: TestClient):
+    """Basic FastAPI version test."""
     response = client.get("/version")
 
     assert response.status_code == status.HTTP_200_OK
     assert "Users API" in response.json()["title"]
+    assert version.__version__ in response.json()["version"]
+
+
+def test_settings(client: TestClient):
+    """Basic FastAPI settings test."""
+    response = client.get("/settings")
+
+    assert response.status_code == status.HTTP_200_OK
