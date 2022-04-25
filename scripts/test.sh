45ff28cef5635f14d91f29ae9658f0f4f40cf531
Damian Bourdin <damian.bourdin@gmail.com>
2022-04-25 10:21:24 +0200

    Added comments and pre-check before running tests.

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,12 +2,17 @@
 
 # Runs the test suite inside a container
 
-
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# Ensure all containers are down
+docker-compose -f "$SCRIPT_DIR"/../docker/docker-compose.yml down
+
+# Run tests
 docker-compose -f "$SCRIPT_DIR"/../docker/docker-compose.yml \
  run \
  --rm \
  users-api \
  pytest --cov="users_api" $@
 
+# Tear down environment
 docker-compose -f "$SCRIPT_DIR"/../docker/docker-compose.yml down
